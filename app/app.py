436dbfd62495c645faf6a6180ff079403e46e9fc
Antonio Gonzalez <aggarcia@gmail.com>
2025-03-05 20:02:13 -0600

    feat: :sparkles: se introduce libreria para generar imagenes dinamicas

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 import json
 import figuras_random
-from flask import Flask, json, render_template
+import dibuja
+from flask import Flask, json, render_template, Response
+
 app = Flask(__name__)
 
 @app.route('/', methods=('GET', 'POST'))
@@ -8,8 +10,8 @@ def home():
     return render_template('index.html')
 
 @app.route('/figuras', methods=('GET', 'POST'))
-def github():
-    return json.dumps({
+def fig():
+    datos={
         "figuras": [
             {
                 "nombre": "cuadrado",
@@ -24,11 +26,22 @@ def github():
                 "lados": 0
             }
         ]
+    }
+    datos['figuras'].append({
+        "nombre": "pentagono",
+        "lados": 5
     })
+    return json.dumps(datos)
     
 @app.route('/figuras_random', methods=('GET', 'POST'))
 def fr():
     return figuras_random.figuras_random()
+
+@app.route('/dibuja', methods=('GET', 'POST'))
+def db():
+    r = Response(response=dibuja.dibuja(), status=200, mimetype="image/png")
+    r.headers["Content-Type"] = "image/png"
+    return r
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
